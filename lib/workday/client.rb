d7bd2a5af1d0ca0f0a802b1359e692de2c4c6509
Jay Wagnon <jay.wagnon@gmail.com>
2013-07-12 14:35:11 -0600

    Added initializer for Client.  Added WSDL for Workday's Human_Resources, version 19

--- lib/workday/client.rb
+++ lib/workday/client.rb
@@ -1,4 +1,21 @@
 module Workday
   class Client
+    def initialize user_name, password
+      @client = Savon.client( initialize_params user_name, password )
+    end
+
+    private
+
+    def initialize_params user_name, password
+      {
+        wsdl: 'lib/workday/assets/human_resources_v19.wsdl',
+        wsse_auth: [user_name, password],
+        namespaces: {'xmlns:bsvc' => 'urn:com.workday/bsvc'},
+        namespace_identifier: :bsvc,
+        log_level: :error,
+        pretty_print_xml: true
+      }
+    end
+
   end
 end
--- spec/workday/client_spec.rb
+++ spec/workday/client_spec.rb
@@ -1,4 +1,18 @@
 require 'spec_helper'
 
 describe Workday::Client do
+
+  describe "#initialize" do
+    it "initializes" do
+      client = Workday::Client.new 'foo', 'bar'
+      client.should be
+    end
+
+    it "assigns a client" do
+      client = Workday::Client.new 'foo', 'bar'
+      client.instance_variable_get(:@client).should be
+    end
+  end
+
+
 end
