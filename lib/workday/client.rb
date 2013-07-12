749ea9afda0b3961db8e72939d32d8324f2068c9
Jay Wagnon <jay.wagnon@gmail.com>
2013-07-12 16:35:36 -0600

    Setting some Client attributes from SOAP response.

--- lib/workday/client.rb
+++ lib/workday/client.rb
@@ -6,17 +6,55 @@ module Workday
       @client = Savon.client( initialize_params user_name, password )
     end
 
+    def get_workers
+      response = @client.call :get_workers, get_workers_params
+      workers_from_response response
+    end
+
     private
+
+    def workers_from_response response
+      workers = []
+
+      if response
+        response.body[:get_workers_response][:response_data][:worker].each do |worker|
+          worker_data = worker[:worker_data]
+          workers << Worker.new(
+            employee_id: worker_data[:worker_id],
+            first_name: worker_data[:personal_data][:name_data][:legal_name_data][:name_detail_data][:first_name],
+            last_name: worker_data[:personal_data][:name_data][:legal_name_data][:name_detail_data][:last_name],
+            hire_date: worker_data[:employment_data][:worker_status_data][:hire_date]
+            )
+        end
+      end
+
+      workers
+    end
 
     def initialize_params user_name, password
       {
-        wsdl: 'lib/workday/assets/human_resources_v19.wsdl',
+        wsdl: File.expand_path('lib/workday/assets/human_resources_v19.wsdl'),
         wsse_auth: [user_name, password],
         namespaces: {'xmlns:bsvc' => 'urn:com.workday/bsvc'},
         namespace_identifier: :bsvc,
         log_level: :error,
         pretty_print_xml: true
       }
+    end
+
+    def get_workers_params
+      {
+        message_tag: :Get_Workers_Request,
+        message: {
+          "bsvc:Response_Group" => {
+            "Include_Personal_Information" => true,
+            "Include_Employment_Information" => true } },
+        attributes: { "bsvc:version" => "v19" }
+      }
+    end
+
+    def worker_employee_id worker
+      worker[:worker_data][:worker_id]
     end
 
   end
--- spec/workday/client_spec.rb
+++ spec/workday/client_spec.rb
@@ -1,15 +1,45 @@
 require 'spec_helper'
 
+require 'webmock/rspec'
+
 describe Workday::Client do
 
-  let(:client){ Workday::Client.new('foo', 'bar') }
+  let(:url){ 'https://i-0af1d538.workdaysuv.com/ccx/service/gms/Human_Resources/v19' }
+  let(:headers){ {'Content-Type' => 'application/xml'} }
+  let(:test_worker_response){ File.read('spec/fixtures/get_workers_response.xml') }
+
+  let(:expected_worker){
+    Workday::Worker.new(
+      employee_id: '21001',
+      first_name: 'Logan',
+      last_name: 'McNeil',
+      hire_date: '2000-01-01-08:00'
+  ) }
+
+  let(:client){ Workday::Client.new('user_name', 'password') }
   subject{client}
 
   it{ should be }
   describe "#initialize" do
     it "assigns a client" do
-      client.instance_variable_get(:@client).should be
+      subject.instance_variable_get(:@client).should be
     end
   end
 
+  it{ should respond_to :get_workers }
+  describe "#get_workers" do
+    it "can retrieve a worker" do
+      stub_request(:post, url).to_return(body: test_worker_response, headers: headers)
+      workers = subject.get_workers
+      workers.size.should eq 100
+
+      # When Virtus releases a fix for ValueObjects with Hashes, these can go
+      # away and be replaced with a simple "should eq" test
+      worker = workers.first
+      worker.employee_id.should eq expected_worker.employee_id
+      worker.first_name.should eq expected_worker.first_name
+      worker.last_name.should eq expected_worker.last_name
+      worker.hire_date.should eq expected_worker.hire_date
+    end
+  end
 end
