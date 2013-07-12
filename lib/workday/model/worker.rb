51a59d91333d54c32e7f8f567c2b918d5a65bb46
Jay Wagnon <jay.wagnon@gmail.com>
2013-07-12 14:02:50 -0600

    Changed worker attributes with types to Hashes.  Worker can't be a ValueObject due to Virtus bug in 00.5.5.

--- lib/workday/model/worker.rb
+++ lib/workday/model/worker.rb
@@ -1,13 +1,15 @@
 module Workday
   class Worker
-    include Virtus::ValueObject
+    include Virtus
+    # include Virtus::ValueObject  -- Bug in 0.5.5 that prevents ValueObject from using Hash coercion; use when fixed
 
     attribute :employee_id, String
     attribute :first_name, String
     attribute :last_name, String
     attribute :hire_date, Date
 
-    attribute :addresses, Set[Address]
-    attribute :email, Email
+    attribute :addresses, Hash[Symbol => Address]
+    attribute :phones, Hash[Symbol => Phone]
+    attribute :emails, Hash[Symbol => Email]
   end
 end
--- spec/workday/model/worker_spec.rb
+++ spec/workday/model/worker_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Workday::Worker do
 
-  %w( employee_id first_name last_name hire_date addresses email ).each do |attribute|
+  %w( employee_id first_name last_name hire_date addresses phones emails ).each do |attribute|
     it{ should have_attribute attribute }
   end
 
