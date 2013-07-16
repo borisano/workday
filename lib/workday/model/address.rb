5b532288afe5cf4663b1f9789f686404a9edcc35
Jay Wagnon <jay.wagnon@gmail.com>
2013-07-16 13:25:36 -0600

    Refactored Address model with factory methods to clean up the client.

--- lib/workday/model/address.rb
+++ lib/workday/model/address.rb
@@ -8,5 +8,37 @@ module Workday
     attribute :state, String
     attribute :postal_code, String
     attribute :country, String
+
+    # Returns a Hash of Address objects parsed from the given Workday response
+    def self.new_from_address_data response
+      addresses = {}
+
+      # The Workday response will have either a single Address_Data element
+      # or an Array of Address_Data elements
+      if response.is_a? Array
+        response.each do |address_data|
+          address = get_address_from_data address_data
+          addresses[address.type] = address
+        end
+      else
+        address = get_address_from_data response
+        addresses[address.type] = address
+      end
+
+      addresses
+    end
+
+    private
+
+    def self.get_address_from_data address_data
+      Address.new(
+        type: address_data[:usage_data][:type_data][:type_reference][:id][1],
+        lines: address_data[:address_line_data].is_a?(Array) ? address_data[:address_line_data] : [address_data[:address_line_data]],
+        city: address_data[:municipality],
+        state: address_data[:country_region_reference][:id][1][/\w+-(\w+)/,1],
+        postal_code: address_data[:postal_code],
+        country: address_data[:country_reference][:id][2]
+      )
+    end
   end
 end
--- spec/workday/model/address_spec.rb
+++ spec/workday/model/address_spec.rb
@@ -6,4 +6,133 @@ describe Workday::Address do
     it{ should have_attribute attribute }
   end
 
+  describe ".new_from_address_data" do
+    let(:address_home){ Workday::Address.new(
+      type: 'HOME',
+      lines: ['42 Laurel Street'],
+      city: 'San Francisco',
+      state: 'CA',
+      postal_code: '94118',
+      country: 'USA' ) }
+
+    let(:address_work){ Workday::Address.new(
+      type: 'WORK',
+      lines: ['3939 The Embarcadero'],
+      city: 'San Francisco',
+      state: 'CA',
+      postal_code: '94118',
+      country: 'USA' ) }
+
+    let(:address_data_one_address){
+      {
+        :country_reference => {
+          :id => [
+            "bc33aa3152ec42d4995f4791a106ed09",
+            "US",
+            "USA",
+            "840"],
+          :"@wd:descriptor" => "United States of America" },
+        :address_line_data => "42 Laurel Street",
+        :municipality => "San Francisco",
+        :country_region_reference => {
+          :id => ["ec3d210e4240442e99a28fa70419aec5", "USA-CA"],
+          :"@wd:descriptor" => "California" },
+        :postal_code => "94118",
+        :usage_data => {
+          :type_data => {
+            :type_reference => {
+              :id => ["836cf00ef5974ac08b786079866c946f", "HOME"],
+              :"@wd:descriptor" => "Home" },
+            :"@wd:primary" => "1" },
+          :"@wd:public" => "0" },
+        :address_reference => {
+          :id => ["91d468fd9c3b465f86ed1362f06b3c5b", "ADDRESS_REFERENCE-4-1"],
+          :"@wd:descriptor" => "ADDRESS_REFERENCE-4-1" },
+        :"@wd:effective_date" => "2008-03-25-07:00",
+        :"@wd:address_format_type" => "Basic",
+        :"@wd:formatted_address" => "42 Laurel Street&#xa;San Francisco, CA 94118&#xa;United States of America",
+        :"@wd:defaulted_business_site_address" => "0"
+      }
+    }
+
+    let(:address_data_multiple_addresses){
+      [
+        {
+          :country_reference => {
+            :id => [
+              "bc33aa3152ec42d4995f4791a106ed09",
+              "US",
+              "USA",
+              "840"],
+            :"@wd:descriptor" => "United States of America" },
+          :address_line_data => "42 Laurel Street",
+          :municipality => "San Francisco",
+          :country_region_reference => {
+            :id => ["ec3d210e4240442e99a28fa70419aec5", "USA-CA"],
+            :"@wd:descriptor" => "California" },
+          :postal_code => "94118",
+          :usage_data => {
+            :type_data => {
+              :type_reference => {
+                :id => ["836cf00ef5974ac08b786079866c946f", "HOME"],
+                :"@wd:descriptor" => "Home" },
+              :"@wd:primary" => "1" },
+            :"@wd:public" => "0" },
+          :address_reference => {
+            :id => ["91d468fd9c3b465f86ed1362f06b3c5b", "ADDRESS_REFERENCE-4-1"],
+            :"@wd:descriptor" => "ADDRESS_REFERENCE-4-1" },
+          :"@wd:effective_date" => "2008-03-25-07:00",
+          :"@wd:address_format_type" => "Basic",
+          :"@wd:formatted_address" => "42 Laurel Street&#xa;San Francisco, CA 94118&#xa;United States of America",
+          :"@wd:defaulted_business_site_address" => "0"
+        },
+        {
+          :country_reference => {
+            :id => [
+              "bc33aa3152ec42d4995f4791a106ed09",
+              "US",
+              "USA",
+              "840"],
+            :"@wd:descriptor" => "United States of America" },
+          :address_line_data => "3939 The Embarcadero",
+          :municipality => "San Francisco",
+          :country_region_reference => {
+            :id => ["ec3d210e4240442e99a28fa70419aec5", "USA-CA"],
+            :"@wd:descriptor" => "California" },
+          :postal_code => "94118",
+          :usage_data => {
+            :type_data => {
+              :type_reference => {
+                :id => ["1f27f250dfaa4724ab1e1617174281e4", "WORK"],
+                :"@wd:descriptor" => "Work" },
+              :"@wd:primary" => "0" },
+            :"@wd:public" => "1" },
+          :address_reference => {
+            :id => ["d77b39b882ec495daed85c9f94876283", "ADDRESS_REFERENCE-4-640"],
+            :"@wd:descriptor" => "ADDRESS_REFERENCE-4-640" },
+          :"@wd:effective_date" => "2000-01-01-08:00",
+          :"@wd:address_format_type" => "Basic",
+          :"@wd:formatted_address" => "3939 The Embarcadero&#xa;San Francisco, CA 94111&#xa;United States of America",
+          :"@wd:defaulted_business_site_address" => "1"
+        },
+      ]
+    }
+
+    context "when response has a single address number" do
+      it "returns a Hash with the address number" do
+        expected_addresses = { 'HOME' => address_home }
+        addresss = Workday::Address.new_from_address_data address_data_one_address
+        addresss.should eq expected_addresses
+      end
+    end
+
+    context "when response has multiple address numbers" do
+      it "returns a Hash with the address numbers" do
+        expected_addresses = { 'HOME' => address_home, 'WORK' => address_work }
+        addresss = Workday::Address.new_from_address_data address_data_multiple_addresses
+        addresss.should eq expected_addresses
+      end
+    end
+  end
+
 end
